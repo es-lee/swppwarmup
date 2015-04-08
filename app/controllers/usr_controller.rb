8ca27fcd0120a461527ef8f08f7f3d33149eb10e
es-lee <dldms800@gmail.com>
2015-04-09 00:26:25 +0900

    error_code json+

--- app/controllers/usr_controller.rb
+++ app/controllers/usr_controller.rb
@@ -7,23 +7,47 @@ class UsrController < ApplicationController
     usr.username = params[:id]
     usr.password = params[:pw]
     usr.logincount = 1
-    usr.save
-
-    render json: {
-      user_name: usr.username,
-      login_count: 1
-    }
+    if usr.save
+      render json: {
+        user_name: usr.username,
+        login_count: 1
+      }
+    else
+      msg = usr.errors.messages
+      if msg[:username] && (msg[:username] != ["has already been taken"])
+        render json: {
+          error_code: -1
+        }
+      elsif usr.errors.messages[:password]
+        render json: {
+          error_code: -2
+        }
+      elsif msg[:username] && (msg[:username] == ["has already been taken"])
+        render json: {
+          error_code: -3
+        }
+      else
+        render json: {
+          error_code: -5
+        }
+      end
+    end
   end
 
   def login
-    usr = User.where(username: params[:id]).first()
-    usr.logincount += 1
-    usr.save
-
-    render json: {
-      user_name: usr.username,
-      login_count: usr.logincount
-    }
+    usr = User.where(username: params[:id], password: params[:pw]).first()
+    if usr
+      usr.logincount += 1
+      usr.save
+      render json: {
+        user_name: usr.username,
+        login_count: usr.logincount
+      }
+    else
+      render json: {
+        error_code: -4
+      }
+    end
   end
 
   def clear
